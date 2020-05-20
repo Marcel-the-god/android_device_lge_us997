9fcbd653ccf643fbc385b6449e9ef7a18b0c80ad
Marcel Walder <59777576+Marcel-the-god@users.noreply.github.com>
2020-05-20 13:52:32 +0000

    AICPify

--- aicp_us997.mk
+++ aicp_us997.mk
@@ -18,15 +18,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+#Bootaniumation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
 
 # Inherit from us997 device
 $(call inherit-product, device/lge/us997/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := us997
-PRODUCT_NAME := lineage_us997
+PRODUCT_NAME := aicp_us997
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US997
 PRODUCT_MANUFACTURER := LGE
